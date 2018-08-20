b66e27ea932ed97323dcdccce3ea0b3f19dddba3
Thomas <tm@pep.security>
2018-08-20 11:32:32 +0200

    Fix bitwise check for flag

--- CpEpEngine.cpp
+++ CpEpEngine.cpp
@@ -1050,7 +1050,7 @@ STDMETHODIMP CpEpEngine::DecryptMessage(TextMessage * src, TextMessage * dst, SA
 
     *flags = (pEpDecryptFlags)engineflags;
 
-    if (engineflags && PEP_decrypt_flag_src_modified)
+    if (engineflags & PEP_decrypt_flag_src_modified)
         text_message_from_C(src, _src);
 
     if (msg_dst)
