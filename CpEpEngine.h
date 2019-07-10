6823df984bdc9163f400318445f4ab854fc8649c
Thomas <tm@pep.security>
2019-07-10 15:08:06 +0200

    COM-99: Comment out release of pointer

--- CpEpEngine.h
+++ CpEpEngine.h
@@ -150,7 +150,7 @@ private:
     {
         for (auto p = sync_callbacks.begin(); p != sync_callbacks.end(); ++p) {
             if (p->cdata) {
-                p->cdata->Release();
+                // p->cdata->Release(); FIXME: This occasionally raises an access violation. To be investigated, why => COM-99
                 p->cdata = nullptr;
             }
         }
